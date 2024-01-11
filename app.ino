feb087e922d6dd56eb780fcdbc15886bd66fb82d
orukdoline <skynets@kakao.com>
2024-01-11 15:14:33 +0900

    Sensor_test

--- app.ino
+++ app.ino
@@ -1,35 +1,11 @@
 #include <DHT.h>
-#include <WiFiS3.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
 
 const int flameSensorPin = 0; // 불꽃 감지 센서 핀
 // const int pirSensorPin = 2; // 인체 감지 센서 핀
 const int irLedPin = 3; // 적외선 송신 모듈 핀
 DHT dht(1, DHT22); // 온습도 센서 핀
 
-char ssid[] = "";    // 와이파이 아이디
-char pass[] = "";    // 와이파이 비밀번호
-
-char mqtt_server[] = "mqtts://fbb1763c.ala.us-east-1.emqxsl.com"; // MQTT 브로커 주소
-char mqtt_port = 8883;
-char mqtt_user[] = "capstone"; // MQTT 아이디
-char mqtt_pass[] = "1234"; // MQTT 비밀번호
-
-const char* ntpServerName = "pool.ntp.org"; // NTP 서버 정보
-const int timeZone = 9;  // 한국 시간대: GMT+9
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-const char publish_topic[] = "fire_detector"; // 토픽
-
 unsigned long lastIRTime = 0; // 점검이 동작했던 시간정보를 저장하기 위한 변수
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, ntpServerName, timeZone * 3600);
 
 void setup() {
   Serial.begin(9600);
@@ -40,45 +16,13 @@ void setup() {
 
   while (!Serial); // 시리얼 통신이 정상적으로 되는지 체크
 
-  // 와이파이 연결
-  Serial.print("Attempting to connect to WPA SSID: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-    Serial.print(".");
-    delay(5000);
-  }
-  Serial.println("You're connected to the network");
-  Serial.println();
-
-  timeClient.begin();
-
-  // MQTT 브로커 연결
-  client.setServer(mqtt_server, mqtt_port);
-  // client.setCallback(callback);
-
-  while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect("ArduinoClient", mqtt_user, mqtt_pass)) {
-      Serial.println("Connected to MQTT broker");
-    } else {
-      Serial.print("Failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" Trying again in 5 seconds...");
-      delay(5000);
-    }
-  }
+  
 }
 
-// void callback(char* topic, byte* payload, unsigned int length) {
-  // 콜백 함수 구현 (필요한 경우)
-// }
-
 void loop() {
-  client.loop();
 
   unsigned long currentTime = millis(); // 현재 시간 정보 저장
   bool isSensorCheck = true;
-  timeClient.update();
 
   // 1시간에 한 번 불꽃감지 센서 점검
   if (currentTime - lastIRTime >= 3600000) {
@@ -86,35 +30,21 @@ void loop() {
     lastIRTime = currentTime; // 점검을 했던 시간 정보 저장
   }
 
-  unsigned long epochTime = timeClient.getEpochTime(); // 현재시간 정보 데이터
+  
   float temperature = dht.readTemperature(); // 온도 센서 데이터
   float humidity = dht.readHumidity(); // 습도 센서 데이터
-  bool isFlameDetected = checkFlame(); // 불꽃감지 센서 데이터
+  isSensorCheck = outputIRSignal();
+  // bool isFlameDetected = checkFlame(); // 불꽃감지 센서 데이터
   //unsigned long isMotionDetected = checkMotion(); // 인체감지 센서 데이터
 
-  // JSON 문서 생성
-  DynamicJsonDocument jsonDocument(200);
+  Serial.print("온도 : ");
+  Serial.println(temperature);
+  Serial.print("습도 : ");
+  Serial.println(humidity);
+  Serial.print("점검결과 : ");
+  Serial.println(isSensorCheck);
 
-  // JSON 문서를 데이터로 채우기
-  jsonDocument["id"] = 1;
-  jsonDocument["name"] = "정보공학관 8층";
-  jsonDocument["temperature"] = temperature;
-  jsonDocument["humidity"] = humidity;
-  jsonDocument["fireDetected"] = isFlameDetected;
-  //jsonDocument["movementDetectedTime"] = isMotionDetected;
-  jsonDocument["checkResult"] = isSensorCheck;
-  jsonDocument["time"] = epochTime;
-
-  // JSON 문자열을 고정할 버퍼
-  char buffer[256];
-
-  // JSON 문서를 문자열로 직렬화
-  serializeJson(jsonDocument, buffer);
-
-  // MQTT 브로커에 JSON 문자열 게시
-  client.publish(publish_topic, buffer);
-
-  delay(10000); // 10초 대기
+  delay(5000); // 10초 대기
 }
 
 // 화재감지 관련 메소드
@@ -142,7 +72,7 @@ bool outputIRSignal() {
   digitalWrite(irLedPin, HIGH); // 적외선 LED 동작
   int flamesensorValue = analogRead(flameSensorPin); // 불꽃감지 센서 동작
   delay(3000); // 3초 대기
-  digitalWrite(irLedPin, LOW); // 적외선 LED 끄기
+  //digitalWrite(irLedPin, LOW); // 적외선 LED 끄기
 
   if (flamesensorValue > threshold) return true;  // 불꽃 센서가 정상작동하면
   else return false; // 불꽃 센서가 오작동하면
